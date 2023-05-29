333ed181acb4d7f57d6da5bce6602e0dde4fc690
Ahmed Khogali <ahmed.khog@outlook.com>
2023-05-29 15:46:33 +0200

    installed navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # ADDED:   1. Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
