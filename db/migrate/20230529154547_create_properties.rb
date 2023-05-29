a5774b4872d729e10bd87524a08eae7dc38773f0
Ahmed Khogali <ahmed.khog@outlook.com>
2023-05-29 18:01:41 +0200

    Made the property class

---
+++ db/migrate/20230529154547_create_properties.rb
@@ -0,0 +1,15 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.integer :capacity
+      t.string :address
+      t.text :summary
+      t.integer :price_per_night
+      t.string :type
+      t.references :user, foreign_key: true
+      t.integer :number_of_rooms
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_132738) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_154547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.integer "capacity"
+    t.string "address"
+    t.text "summary"
+    t.integer "price_per_night"
+    t.string "type"
+    t.bigint "user_id"
+    t.integer "number_of_rooms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_132738) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
