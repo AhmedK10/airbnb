cdb980cf259e035afea818691262b14e57bca919
onur deryahanoglu <onur.deryahan@gmail.com>
2023-05-30 14:05:19 +0200

    review section2 completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,5 +3,18 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
   
- def create
- end
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to @booking
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:id, :comment, :rating)
+  end
+end
