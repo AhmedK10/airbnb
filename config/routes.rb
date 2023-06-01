201aa726e18a19378e685c5501aad129fa0f0e05
onur deryahanoglu <onur.deryahan@gmail.com>
2023-06-01 14:15:31 +0200

    reviews design completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookings, only: %i[] do
+    resources :reviews, only: [:new, :create]
+  end
 
   resources :properties, only: %i[index show] do
-    resources :bookings, only: %i[index new create] do
-      resources :reviews, only: [:new, :create]
-    end
+    resources :bookings, only: %i[index new create] 
   end
   resources :favorites, only: %i[index create]
 end
