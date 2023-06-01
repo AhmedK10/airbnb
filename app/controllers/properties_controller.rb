b1e7fc941f39cb15b73c345dfbd9300b98fa3508
Jasmeet Samra <js.samra@outlook.com>
2023-06-01 14:39:10 +0200

    search bar enabled filter by price

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,7 +5,13 @@ class PropertiesController < ApplicationController
     @properties = Property.all
 
     if params[:query]
-      @properties = @properties.where(address: params[:query])
+      @properties = @properties.where("address ILIKE ?", "%#{params[:query]}%")
+    end
+
+    if params[:min_price].present? && params[:max_price].present?
+      min_price = params[:min_price].to_i
+      max_price = params[:max_price].to_i
+      @properties = @properties.where(price_per_night: min_price..max_price)
     end
   end
 
