b091055e2995a338330b6ac6c8db575a0fee98b1
Jasmeet Samra <js.samra@outlook.com>
2023-06-01 13:44:35 +0200

    implemented working search bar with front end

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,12 @@
 class PropertiesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-  
+
   def index
     @properties = Property.all
+
+    if params[:query]
+      @properties = @properties.where(address: params[:query])
+    end
   end
 
   def show
